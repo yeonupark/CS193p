cca1f85c1ec7226daaa0a1ddc8ce6111fa7d01c7
yeonupark <idepix@naver.com>
2025-03-04 10:52:55 +0900

    git init

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Yeonu Park on 2025/03/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Yeonu Park on 2025/03/04.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
